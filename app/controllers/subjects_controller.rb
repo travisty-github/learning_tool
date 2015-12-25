1812d38341d93606555eddca94c73e589149564d
Travis <bloomfield.travis@gmail.com>
2015-12-25 23:00:13 +0000

    Created subject test page.

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -3,5 +3,9 @@ class SubjectsController < ApplicationController
   def index
     @subjects = Subject.all
   end
-  
+
+  def test
+    @chunks = Chunk.where(subject_id: params[:subject_id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :subjects do
     resources :chunks
   end
+  get 'subjects/:subject_id/test' => 'subjects#test', as: :test
   root 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
