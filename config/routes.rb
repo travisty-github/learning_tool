6cbaff1aaf71ec0d34d29efd87d9a8d801e09197
Travis <bloomfield.travis@gmail.com>
2015-12-25 20:04:05 +0000

    Added views for subjects and chunks.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+  resources :subjects do
+    resources :chunks
+  end
   root 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
