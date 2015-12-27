04857ef13dfafb1e6529a2a6a04040e06aeb13db
Travis <bloomfield.travis@gmail.com>
2015-12-27 14:09:33 +0000

    Added show/hide functionality to test buttons.

--- app/assets/javascripts/paloma/subjects.js
+++ app/assets/javascripts/paloma/subjects.js
@@ -2,8 +2,13 @@ var SubjectsController = Paloma.controller('Subjects');
 
 // Executes when Rails SubjectsController#test is executed.
 SubjectsController.prototype.test = function(){
-  alert('inside controller');
-  // $('document').ready = function() {
-  //   alert('ready');
-  // }
+  $('document').ready(function() {
+
+    // Show answer, hide answer button and show correct/false buttons
+    $('.test-btn-answer').click(function() {
+      $(this).prevAll('.test-answer').removeClass('hidden');
+      $(this).nextAll('button').removeClass('hidden');
+      $(this).addClass('hidden');
+    });
+  });
 };
