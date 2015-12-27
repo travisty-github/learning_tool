e8f0c1da5719973a0114946b55e88cae24e6fc0d
Travis <bloomfield.travis@gmail.com>
2015-12-27 15:04:06 +0000

    Implemented correct/incorrect answer butttons.

--- app/assets/javascripts/paloma/subjects.js
+++ app/assets/javascripts/paloma/subjects.js
@@ -4,11 +4,29 @@ var SubjectsController = Paloma.controller('Subjects');
 SubjectsController.prototype.test = function(){
   $('document').ready(function() {
 
-    // Show answer, hide answer button and show correct/false buttons
+    // Show answer, hide answer button and show correct/incorrect buttons
     $('.test-btn-answer').click(function() {
       $(this).prevAll('.test-answer').removeClass('hidden');
       $(this).nextAll('button').removeClass('hidden');
       $(this).addClass('hidden');
+    });
+
+    // Correct answer
+    $('.test-btn-correct').click(function() {
+      $(this).parent().addClass('correct-answer-well');
+      $(this).parent().removeClass('well');
+      // Hide all buttons
+      $(this).addClass('hidden');
+      $(this).next().addClass('hidden');
+      // TODO AJAX response
+    });
+
+    // Incorrect answer
+    $('.test-btn-incorrect').click(function() {
+      // Hide all buttons
+      $(this).addClass('hidden');
+      $(this).prev().addClass('hidden');
+      // TODO AJAX response
     });
   });
 };
